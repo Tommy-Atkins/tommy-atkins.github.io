c9b99ffee7664aba4b9883ec71c035b3509f034c
Tommy-Atkins <slushayekzorcist@gmail.com>
2017-02-03 21:09:51 +0200

    upload files
    test

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+var link = document.querySelector(".content-right .btn");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+
+link.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.add("modal-content-show");
+});
+
+close.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.remove("modal-content-show");
+});
